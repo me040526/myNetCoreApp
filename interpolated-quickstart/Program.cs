54f53b95973b0570e22198da091dc4027b035108
me040526 <469231573@qq.com>
2018-03-16 16:58:05 +0800

    1.增加AirportBaggageClaimInfoSystem项目（机场行李认领信息系统）； 2.增加FibonacciNumber项目（斐波那契数列）； 3.增加interpolated-quickstart项目（可插值字符串）;

---
+++ interpolated-quickstart/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace interpolated_quickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Jim";
+            Console.WriteLine($"Hello, {name}. It's a pleasure to meet you!");
+            Console.Read();
+        }
+    }
+}
